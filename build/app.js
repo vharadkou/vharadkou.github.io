121193f2659838e6510579f558fa57baa47ebc03
VHaradkou <valicser@yandex.ru>
2016-12-05 01:47:08 +0300

    added build folder

---
+++ build/app.js
@@ -0,0 +1,121 @@
+webpackJsonp([0],{
+
+/***/ 0:
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	var platform_browser_dynamic_1 = __webpack_require__(1);
+	var app_module_1 = __webpack_require__(23);
+	platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
+
+
+/***/ },
+
+/***/ 23:
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+	    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+	    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+	    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+	    return c > 3 && r && Object.defineProperty(target, key, r), r;
+	};
+	var __metadata = (this && this.__metadata) || function (k, v) {
+	    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+	};
+	var core_1 = __webpack_require__(3);
+	var platform_browser_1 = __webpack_require__(21);
+	var containers_1 = __webpack_require__(24);
+	var AppModule = (function () {
+	    function AppModule() {
+	    }
+	    AppModule = __decorate([
+	        core_1.NgModule({
+	            imports: [
+	                platform_browser_1.BrowserModule
+	            ],
+	            declarations: [
+	                containers_1.WeatherInfoComponent
+	            ],
+	            bootstrap: [containers_1.WeatherInfoComponent]
+	        }), 
+	        __metadata('design:paramtypes', [])
+	    ], AppModule);
+	    return AppModule;
+	}());
+	exports.AppModule = AppModule;
+
+
+/***/ },
+
+/***/ 24:
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	function __export(m) {
+	    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+	}
+	__export(__webpack_require__(25));
+
+
+/***/ },
+
+/***/ 25:
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	var weather_info_component_1 = __webpack_require__(26);
+	exports.WeatherInfoComponent = weather_info_component_1.WeatherInfoComponent;
+
+
+/***/ },
+
+/***/ 26:
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+	    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+	    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+	    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+	    return c > 3 && r && Object.defineProperty(target, key, r), r;
+	};
+	var __metadata = (this && this.__metadata) || function (k, v) {
+	    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+	};
+	var core_1 = __webpack_require__(3);
+	var WeatherInfoComponent = (function () {
+	    function WeatherInfoComponent() {
+	    }
+	    WeatherInfoComponent = __decorate([
+	        core_1.Component({
+	            selector: 'weather-info',
+	            template: __webpack_require__(27),
+	            styles: [__webpack_require__(28)]
+	        }), 
+	        __metadata('design:paramtypes', [])
+	    ], WeatherInfoComponent);
+	    return WeatherInfoComponent;
+	}());
+	exports.WeatherInfoComponent = WeatherInfoComponent;
+
+
+/***/ },
+
+/***/ 27:
+/***/ function(module, exports) {
+
+	module.exports = "<h1>Weather Info</h1>";
+
+/***/ },
+
+/***/ 28:
+/***/ function(module, exports) {
+
+	module.exports = "h1 {\r\n    color: mediumpurple;\r\n}"
+
+/***/ }
+
+});
+//# sourceMappingURL=app.js.map
